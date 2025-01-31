2d90894a39858dd9ec026c069accc133f17a6946
streetcodec <streetcodec@gmail.com>
2025-01-31 22:15:54 +0530

    corrected api 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
     proxy: {
-      '/api': {
-        target: 'https://fastapimanagement.onrender.com',
-        changeOrigin: true,
-      },
+      '/api': 'https://fastapimanagement.onrender.com'
+        
+      
     },
   },
 }) 
